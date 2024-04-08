23d1fe2357308b9b41ca0b600c82761d79a7dc67
Gustavo <gustavotrigolo8@gmail.com>
2024-04-08 10:28:16 -0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Haykuu from "./components/Haykuu"
 import Person from "./components/Person"
+import Studio from "./components/Studio"
 
 export default function App() {
   return (
-    <main>
+    <main >
       <Haykuu/>
       <Person/>
+      <Studio/>
     </main>
    
   )
